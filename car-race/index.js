b3aec92adafa121ef38a5ad436f9ff95cd834805
Mayank Kumrawat <mkmayank1806@gmail.com>
2021-12-12 12:10:37 +0530

    initial commit

---
+++ car-race/index.js
@@ -0,0 +1,208 @@
+const score = document.querySelector('.score');
+const startScreen = document.querySelector('.startScreen');
+const gameArea = document.querySelector('.gameArea');
+
+let player = { start: false, speed: 8, score: 0 };
+
+startScreen.addEventListener('click', start);
+
+let keys = { ArrowUp: false, ArrowDown: false, ArrowRight: false, ArrowLeft: false }
+
+
+
+//---------------------------------------------
+
+// if (screen.availWidth <= 810 || screen.width <= 810){
+let control = document.createElement("div")
+let controlBox = document.getElementById("controlbox")
+control.innerHTML = `<button class="button" id="top"><i class="fas fa-angle-double-up"></i></button>
+    <div>
+        <button  class="button"  id="left"></button>
+        <button  class="button"  id="right"></button>
+    </div>
+    <button  class="button"  id="bottom"><i class="fas fa-angle-double-down"></i></button>`
+controlBox.appendChild(control)
+//}
+
+let leftb = document.querySelector("#left")
+let rightb = document.querySelector("#right")
+let bottomb = document.querySelector("#bottom")
+let topb = document.querySelector("#top")
+
+leftb.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keys.ArrowLeft = true
+})
+leftb.addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keys.ArrowLeft = false
+})
+
+
+rightb.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keys.ArrowRight = true
+})
+rightb.addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keys.ArrowRight = false
+})
+
+topb.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keys.ArrowUp = true
+})
+topb.addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keys.ArrowUp = false
+})
+
+bottomb.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    keys.ArrowDown = true
+})
+bottomb.addEventListener("touchend", (e) => {
+    e.preventDefault();
+    keys.ArrowDown = false
+})
+
+
+document.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    keys[e.key] = true;
+    // console.log(e.key);
+});
+
+document.addEventListener('keyup', (e) => {
+    e.preventDefault();
+    keys[e.key] = false;
+    // console.log(e.key);
+
+});
+
+//==============================================================
+
+
+//==============================================================
+
+function isCollide(a, b) {
+    aRect = a.getBoundingClientRect();
+    bRect = b.getBoundingClientRect();
+    return !((aRect.top > bRect.bottom) || (aRect.bottom < bRect.top) || (aRect.left > bRect.right) || (aRect.right < bRect.left))
+}
+
+function movelines() {
+    let lines = document.querySelectorAll('.lines');
+
+    lines.forEach(item => {
+        if (item.y >= 600) {
+            item.y -= 750;
+        }
+        item.y += player.speed;
+        item.style.top = item.y + 'px';
+    });
+}
+
+function endGame() {
+    player.start = false;
+    startScreen.classList.remove('hide');
+    startScreen.innerHTML = "Game Over <br> Your Score:" + player.score + "<br> Press Here to restart."
+}
+
+
+function moveEnemy(car) {
+    let enemy = document.querySelectorAll('.enemy');
+
+    enemy.forEach(item => {
+
+        if (isCollide(car, item)) {
+            // console.log('You Hit The Car');
+            endGame();
+        }
+
+        if (item.y >= 580) {
+            item.y -= 1000;
+            item.style.left = Math.round(350 * Math.random()) + 'px';
+
+        }
+        // item.y += player.speed;
+        item.y += player.speed - 3;
+        item.style.top = item.y + 'px';
+    });
+}
+
+function gamePlay() {
+    let car = document.querySelector('.car')
+    let road = gameArea.getBoundingClientRect();
+    // console.log(road);
+
+    if (player.start) {
+
+        movelines();
+        moveEnemy(car);
+
+        if (keys.ArrowUp && player.y > road.top + 40) { player.y -= player.speed; }
+        if (keys.ArrowDown && player.y < road.bottom - 100) { player.y += player.speed; }
+        if (keys.ArrowLeft && player.x > 0) { player.x -= player.speed; }
+        if (keys.ArrowRight && player.x < road.width - 63) { player.x += player.speed; }
+
+
+        car.style.top = player.y + 'px';
+        car.style.left = player.x + 'px';
+
+        window.requestAnimationFrame(gamePlay);
+        // console.log(player.score++);
+        score.innerText = "Score: " + player.score++;
+    }
+}
+
+function start() {
+    // gameArea.classList.remove('hide')
+    startScreen.classList.add('hide')
+    gameArea.innerHTML = "";
+    player.start = true;
+    player.score = 0;
+    window.requestAnimationFrame(gamePlay);
+
+    for (x = 0; x < 5; x++) {
+        let roadline = document.createElement('div');
+        roadline.setAttribute('class', 'lines');
+        roadline.y = (x * 150);
+        roadline.style.top = roadline.y + 'px';
+        gameArea.appendChild(roadline);
+    }
+
+    let car = document.createElement('div')
+    car.setAttribute('class', 'car');
+    gameArea.appendChild(car);
+
+    player.x = car.offsetLeft;
+    player.y = car.offsetTop;
+
+    //     console.log('top',car.offsetTop);
+    //     console.log('left',car.offsetLeft);
+
+    for (x = 0; x < 4; x++) {
+        let enemyCar = document.createElement('div');
+        enemyCar.setAttribute('class', 'enemy');
+        enemyCar.y = ((x) * 250) * -1;
+        enemyCar.style.top = enemyCar.y + 'px';
+        // enemyCar.style.backgroundColor = randomColor();
+        enemyCar.style.left = Math.round(350 * Math.random()) + 'px';
+        gameArea.appendChild(enemyCar);
+    }
+}
+
+function randomColor() {
+    function c() {
+        let hex = Math.round(Math.random() * 256).toString(16);
+        return ("0" + hex).substr(-2);
+    }
+    return '#' + c() + c() + c();
+}
+
+
+
+
+
+
---
+++ meteorite-hunter/desktop.js
@@ -0,0 +1,255 @@
+const score = document.querySelector('#score');
+const missed = document.querySelector('#missed');
+const startScreen = document.querySelector('#startScreen');
+const gameSpace = document.querySelector('#gameSpace');
+let gameSpaceRect = gameSpace.getBoundingClientRect();
+
+//========================================
+
+let player = { start: false, speed: 15, score: 0, fire: false, missed: 0, music: true, paused: false };
+
+let rocketMotion = { y: 0, number: 1 }
+
+let keys = { ArrowUp: false, ArrowRight: false, ArroLeft: false }
+
+let collideInfo = { isCollide: false, asteroidName: null }
+
+//======================================
+
+const audioButton = document.querySelector('#bgm');
+let bgm = new Audio('bgm.mp3');
+let fireAudio = new Audio('rocketFire.mp3');
+
+audioButton.addEventListener('click', () => {
+    if (player.music) {
+        player.music = false;
+        audioButton.innerHTML = `<i class="fas fa-volume-mute"></i>`;
+
+    } else {
+        player.music = true;
+        audioButton.innerHTML = `<i class="fas fa-music"></i>`;
+
+    }
+})
+
+//=====================================
+
+startScreen.addEventListener('click', start);
+
+//=======================================
+
+document.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    keys[e.key] = true;
+});
+
+document.addEventListener('keyup', (e) => {
+    e.preventDefault()
+    keys[e.key] = false;
+});
+
+document.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    if (keys[e.key == "ArrowUp"])
+        keys.ArrowUp = true;
+})
+
+//===========================================
+
+function isCollide(launcher, asteroid) {
+    aRect = launcher.getBoundingClientRect();
+    bRect = asteroid.getBoundingClientRect();
+    return !((aRect.top > bRect.bottom) || (aRect.bottom < bRect.top) || (aRect.left > bRect.right) || (aRect.right < bRect.left))
+}
+
+function isCollideWithRocket(rocket, asteroids) {
+    rocketRect = rocket.getBoundingClientRect();
+    for (i = 0; i < 4; i++) {
+        asteroidRect = asteroids[i].getBoundingClientRect();
+        collideInfo.asteroidName = asteroids[i];
+        collideInfo.isCollide = !((rocketRect.top > asteroidRect.bottom) || (rocketRect.bottom < asteroidRect.top) || (rocketRect.left > asteroidRect.right) || (rocketRect.right < asteroidRect.left))
+        if (collideInfo.isCollide) {
+            break;
+        }
+    }
+
+}
+
+//==========================================
+
+function blast(asteroid) {
+    asteroid.classList.add('hide');
+}
+
+//==========================================
+
+function end() {
+    bgm.pause();
+    player.start = false;
+    startScreen.classList.add('startScreenAnimation');
+    startScreen.innerHTML = `Game Over <br>
+    Your score: ${player.score} <br>
+    Press Here to restart.`
+}
+
+//===========================================
+
+function moveAsteroid() {
+    let asteroids = document.querySelectorAll('.asteroids');
+    let rocket = document.querySelector(`#rocket${rocketMotion.number}`);
+    asteroids.forEach(element => {
+        if (/*(isCollide(launcher, element)) ||*/ (player.missed == 10)) {
+            end();
+
+        }
+        if (element.y >= 580) {
+            element.y -= 1000;
+            if (!element.classList.contains('hide')) {
+                player.missed++
+            }
+            element.classList.remove('hide');
+            element.style.left = Math.round((gameSpaceRect.width - 90) * Math.random()) + 'px';
+        }
+        element.y += (player.speed + (Math.round(player.score / 15)) - 12);
+        element.style.top = element.y + 'px';
+    });
+
+
+    if (player.fire) {
+        let asteroids = document.querySelectorAll('.asteroids');
+        isCollideWithRocket(rocket, asteroids);
+
+        if (collideInfo.isCollide) {
+            player.score++;
+            blast(collideInfo.asteroidName);
+            collideInfo.asteroidName = null;
+            collideInfo.isCollide = false;
+        }
+
+        if (rocketMotion.y <= -220) {
+            gameSpace.removeChild(gameSpace.childNodes[1]);
+            let newRocket = document.createElement('div');
+            newRocket.setAttribute('class', 'rocket');
+            newRocket.classList.add('hide');
+            newRocket.setAttribute('id', `rocket${rocketMotion.number++}`);
+            gameSpace.insertBefore(newRocket, gameSpace.children[4]);
+            player.fire = false;
+        }
+
+        else {
+            rocketMotion.y -= (player.speed + 10);
+            rocket.style.top = rocketMotion.y + 'px';
+        }
+
+    }
+}
+
+//==========================================
+
+function moveRocket() {
+    if (rocketMotion.number == 5) {
+        rocketMotion.number = 1;
+    }
+
+    let rocket = document.querySelector(`#rocket${rocketMotion.number}`);
+    rocket.classList.remove('hide');
+    rocket.style.left = player.x + 'px';
+    rocketMotion.y = player.y;
+    player.fire = true;
+
+    if (player.music) {
+        fireAudio.play();
+    }
+}
+//=========================================
+
+function gamePlay() {
+    let launcher = document.querySelector('#launcher');
+    if (player.start) {
+        moveAsteroid();
+
+        if (keys.ArrowLeft && player.x > 0) {
+            player.x -= player.speed;
+        }
+        if (keys.ArrowRight && (player.x + 50 + 23) < (gameSpaceRect.width)) {
+            player.x += player.speed;
+        }
+        if (keys.ArrowUp) {
+            fireButton.click();
+        }
+        keys.ArrowUp = false;
+        if (player.music) {
+            bgm.play();
+        } else {
+            bgm.pause();
+        }
+
+        //===================================
+
+        if (bgm.ended) {
+            bgm.currentTime = 0;
+            bgm.play()
+        }
+
+        //====================================
+
+        launcher.style.left = player.x + 'px';
+        window.requestAnimationFrame(gamePlay);
+
+
+        score.innerHTML = 'Score: ' + player.score;
+        missed.innerHTML = 'Missed: ' + player.missed;
+    }
+}
+
+//=========================================
+
+function start() {
+    startScreen.setAttribute('class', 'startScreenBegin');
+    startScreen.classList.remove('startScreenAnimation');
+    gameSpace.innerHTML = '';
+    player.start = true;
+    player.score = 0;
+    player.missed = 0;
+    rocketMotion.number = 1;
+    bgm.currentTime = 0;
+    window.requestAnimationFrame(gamePlay);
+
+    //=====================================
+
+    let launcher = document.createElement('div');
+    launcher.setAttribute('id', 'launcher');
+    gameSpace.appendChild(launcher);
+
+    player.x = launcher.offsetLeft;
+    player.y = launcher.offsetTop;
+    //===================================
+
+    for (i = 0; i < 4; i++) {
+        let rocket = document.createElement('div');
+        rocket.setAttribute('class', 'rocket');
+        rocket.setAttribute('id', `rocket${i + 1}`)
+        rocket.classList.add('hide');
+        gameSpace.appendChild(rocket);
+    }
+
+    //===================================
+
+    let fireButton = document.createElement('div');
+    fireButton.setAttribute('id', 'fireButton');
+    gameSpace.appendChild(fireButton);
+    fireButton.addEventListener('click', moveRocket);
+
+    //======================================
+
+    for (i = 0; i < 4; i++) {
+        let asteroid = document.createElement('div');
+        asteroid.setAttribute('class', 'asteroids');
+        asteroid.y = - ((i) * 250);
+        asteroid.style.top = asteroid.y + 'px';
+        asteroid.style.left = Math.round(Math.random() * (gameSpaceRect.width - 90)) + 'px';
+        gameSpace.appendChild(asteroid);
+    }
+
+}
+
